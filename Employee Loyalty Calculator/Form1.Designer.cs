9569ee2bb82bfefa5a1f784b55498a0a396f12db
Waldo881 <137924298+Waldo881@users.noreply.github.com>
2026-07-13 23:09:34 +0200

    Add files via upload

---
+++ Employee Loyalty Calculator/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace vanderWaltPrac7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtNoYW = new System.Windows.Forms.TextBox();
+            this.txtPrevDebt = new System.Windows.Forms.TextBox();
+            this.lstLoyalty = new System.Windows.Forms.ListBox();
+            this.lstDebt = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Emp name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "No of years worked: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Previous debt:";
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(186, 55);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(100, 22);
+            this.txtEmpName.TabIndex = 3;
+            // 
+            // txtNoYW
+            // 
+            this.txtNoYW.Location = new System.Drawing.Point(186, 96);
+            this.txtNoYW.Name = "txtNoYW";
+            this.txtNoYW.Size = new System.Drawing.Size(100, 22);
+            this.txtNoYW.TabIndex = 4;
+            // 
+            // txtPrevDebt
+            // 
+            this.txtPrevDebt.Location = new System.Drawing.Point(186, 129);
+            this.txtPrevDebt.Name = "txtPrevDebt";
+            this.txtPrevDebt.Size = new System.Drawing.Size(100, 22);
+            this.txtPrevDebt.TabIndex = 5;
+            // 
+            // lstLoyalty
+            // 
+            this.lstLoyalty.FormattingEnabled = true;
+            this.lstLoyalty.ItemHeight = 16;
+            this.lstLoyalty.Location = new System.Drawing.Point(54, 196);
+            this.lstLoyalty.Name = "lstLoyalty";
+            this.lstLoyalty.Size = new System.Drawing.Size(420, 228);
+            this.lstLoyalty.TabIndex = 6;
+            // 
+            // lstDebt
+            // 
+            this.lstDebt.FormattingEnabled = true;
+            this.lstDebt.ItemHeight = 16;
+            this.lstDebt.Location = new System.Drawing.Point(492, 196);
+            this.lstDebt.Name = "lstDebt";
+            this.lstDebt.Size = new System.Drawing.Size(480, 228);
+            this.lstDebt.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 455);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 68);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate Take Home";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 455);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 68);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "New Employee";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(518, 455);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 68);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 546);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstDebt);
+            this.Controls.Add(this.lstLoyalty);
+            this.Controls.Add(this.txtPrevDebt);
+            this.Controls.Add(this.txtNoYW);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TimTam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtNoYW;
+        private System.Windows.Forms.TextBox txtPrevDebt;
+        private System.Windows.Forms.ListBox lstLoyalty;
+        private System.Windows.Forms.ListBox lstDebt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Employee Loyalty Calculator/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace vanderWaltPrac7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txtEmpName.Text;
+                int yearsWorked;
+                int.TryParse(txtEmpName.Text, out yearsWorked);
+                int totalDebt;
+                int.TryParse(txtPrevDebt.Text, out totalDebt);  
+                
+
+               
+                
+                    if(yearsWorked < 10 && yearsWorked > 0) // checking if years worked is less than 10 and bigger than 0.
+                    {
+                        lstLoyalty.Items.Add("Dear " + name + "thank you for you loyal service");
+
+                        int loyaltyBonus = 0;
+
+
+                        for (int i = 1; i <= yearsWorked; ++i) // for loop to 
+                        {
+                            loyaltyBonus += 1000;
+
+                        lstLoyalty.Items.Add("For year " + i + " your loyalty bonus is added and is now " + loyaltyBonus);
+                            
+                        }
+
+                    
+
+                    
+                   if (totalDebt < yearsWorked *1000) // checking if debt is smaller than years worked.
+                    {
+                        lstDebt.Items.Add("You owe the company " + totalDebt + ". For each year 500 is deducted to pay debt.");
+                        int loyaltyBonus2 = yearsWorked *1000;
+                        int j = 1;
+
+                        while (totalDebt >= 500) // checking if debt is bigger than 500 then doing neccacary calculations to print the debt after each year
+                        {
+                            totalDebt -= 500;
+                            loyaltyBonus2 -= 500;
+
+                            lstDebt.Items.Add("New debt after year" + j + " debt is " + totalDebt);
+                            lstDebt.Items.Add("New loyalty after year" + j + " debt is " + loyaltyBonus2);
+
+                            j++;
+
+                        }
+                        lstDebt.Items.Add("Company will write off remaining " + totalDebt + " on your debt");
+                        totalDebt = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Enter debt amount lower than your loyalty bonus.");
+                    }
+
+                }
+                    else if(yearsWorked > 9) // checking if years worked thengiving neccecary bonusses
+                {
+                    lstLoyalty.Items.Add("Dear " + name + "thank you for you loyal service");
+
+                    int loyaltyBonus = 0;
+
+
+                    for (int i = 1; i <= yearsWorked; ++i)// for loop to iterate through the years
+                    {
+                        loyaltyBonus += 2000;
+
+                        lstLoyalty.Items.Add("For year " + i + " your loyalty bonus is added and is now " + loyaltyBonus);
+
+                    }
+
+                    lstDebt.Items.Add("You owe the company " + totalDebt + ". For each year 500 is deducted to repay debt");
+
+                   if(totalDebt < yearsWorked * 2000)
+                    {
+                        lstDebt.Items.Add("You owe the company " + totalDebt + ". For each year 500 is deducted to pay debt.");
+                        int loyaltyBonus2 = yearsWorked * 2000;
+                        int j = 1;
+
+                        while (totalDebt >= 500) // while loop to check debt and do neccacary calculations
+                        {
+                            totalDebt -= 500;
+                            loyaltyBonus2 -= 500;
+
+                            lstDebt.Items.Add("New debt after year" + j + " debt is " + totalDebt);
+                            lstDebt.Items.Add("New loyalty after year" + j + " debt is " + loyaltyBonus2);
+
+                            j++;
+
+                        }
+                        lstDebt.Items.Add("Company will write off remaining " + totalDebt + " on your debt");
+                        totalDebt = 0;
+                    }
+                    else// 
+                    {
+                        MessageBox.Show("Enter debt amount lower than your loyalty bonus.");
+                    }
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("No employers work here less than one year.");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error."); // when error occurs
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtEmpName.Clear();
+            txtNoYW.Clear();
+            txtPrevDebt.Clear();
+            lstDebt.Items.Clear();
+            lstLoyalty.Items.Clear();
+        }
+    }
+}
